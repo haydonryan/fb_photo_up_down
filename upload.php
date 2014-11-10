faabf6b0cd57cda71048676946b54109040b4cdb
Haydon Ryan <haydon.ryan@gmail.com>
2014-11-09 18:21:14 -0800

    Functionised page login, and attempted to create updatedate
    UpdateDate does not currently work.

--- upload.php
+++ upload.php
@@ -21,12 +21,10 @@ use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
 
-$FBPAGE_ID = '191589877477';  //Amnesia Photos page
-
+//$FBPAGE_ID = '191589877477';  //Amnesia Photos page
+$FBPAGE_ID = '629795797083447';  //retarded commedy page
 
 print "Starting up\n";
-
-
 print "Initialising SDK...";
 #session_start();
 print "Setting App ID: $APP_ID\n";
@@ -38,11 +36,21 @@ print("Go To a browser and type in:\n");
 print("https://www.facebook.com/dialog/oauth?client_id=$APP_ID&redirect_uri=https%3A%2F%2Fwww.amnesiaphotos.com%2Fconnect%2Flogin_success.html&scope=publish_stream,manage_pages,user_status&response_type=token\n");
 
 // If you're making app-level requests:
-$session = FacebookSession::newAppSession();
+//$session = FacebookSession::newAppSession();
 
 // this has to come from the parameter sent to amnesia photos site
 $session = new FacebookSession('CAAAACzWJ7UIBAOlkVg5ZCpzlGYgTHJ5wpZC3KukghQAgphH3rlNzmRjdn0wYznbJ15NPIIHQ1pxDp2WRdCh4DoDj9btdVPfwzYfSmwZCUbu8IUZAal5k0tYBdU8wTafTDCYjPMAiKpa35BhPkXZC1gcs9yTyNmZBfabryYFUsKMEJjKYf3wgt7uXS65tPaw9hVGNLkVEF34FoZChXa3muw3');
 
+
+$session =  LoginToFacebookPage ($session, $FBPAGE_ID );
+
+$album = CreateAlbum ( $session, 'Test Album', 'test' );
+//SetDate($session, $FBPAGE_ID, $page_access_token, $album,'2013-03-06T15:18:26-08:00');
+UploadPhoto($session, $album, '504708006080.jpg');
+
+function LoginToFacebookPage ($session, $FBPAGE_ID )
+{
+//echo "Creating Album, $title\n";
 // To validate the session:
 try {
   $session->validate();
@@ -80,17 +88,11 @@ if($session) {
 
 $session = new FacebookSession($page_access_token);
 
-CreateAlbum ( $session, 'Test Album', 'test' );
 
-/*
-function LoginToFacebook ($title, $description )
-{
-echo "Creating Album, $title\n";
-
-return 1;
+return $session;
 }
-*/
-function UploadPhoto ( $session, $albumid,  $title, $description )
+
+function UploadPhoto ( $session, $albumid, $filename )
 {
 	echo "Creating Album, $title\n";
 
@@ -105,9 +107,12 @@ function UploadPhoto ( $session, $albumid,  $title, $description )
 		 	echo 'trying:';
 			print '/'+$FBPAGE_ID+'/photos'; 
 
-		   $response = (new FacebookRequest(      $session, 'POST', '/me/photos', array(
-			'source' => new CURLFile('504708006080.jpg', 'image/jpg'),
-			'message' => 'User provided message'
+		   $response = (new FacebookRequest(      $session, 'POST', "/$albumid/photos", array(
+			'source' => new CURLFile($filename, 'image/jpg'),
+			'message' => 'User provided message',
+			'backdated_time' => '2013-03-06T15:18:26',
+			'created_time' => '2013-03-06T15:18:26',
+			'updated_time' => '2013-03-06T15:18:26'
 		      )
 		    ))->execute()->getGraphObject();
 
@@ -124,6 +129,40 @@ function UploadPhoto ( $session, $albumid,  $title, $description )
 	return 1;
 }
 
+
+function SetDate ( $session, $FBPAGE_ID, $page_access_token, $id, $date )
+{
+
+	echo "Creating Album, $title\n";
+
+	if($session) {
+		  try {
+		    // Upload to a user's profile. The photo will be in the
+		    // first album in the profile. You can also upload to
+		    // a specific album by using /ALBUM_ID as the path     
+	echo "/$FBPAGE_ID/$id";
+		  $response = (new FacebookRequest(      $session, 'POST', "/album/$id", array(
+			'backdated_time' => '2013-03-06T15:18:26',
+			'created_time' => '2013-03-06T15:18:26',
+			'updated_time' => '2013-03-06T15:18:26',
+			'access_token' => $page_access_token		      
+)
+		    ))->execute()->getGraphObject();
+
+		    echo "Posted with id: " . $response->getProperty('id') . "\n";
+			return $response->getProperty('id');
+		  } catch(FacebookRequestException $e) {
+
+		    echo "Exception occured, code: " . $e->getCode() . "\n";
+		    echo " with message: " . $e->getMessage() . "\n";
+
+		  }   
+
+	}
+	return 0;
+}
+
+
 function CreateAlbum ( $session, $albumName, $description )
 {
 
@@ -137,10 +176,12 @@ function CreateAlbum ( $session, $albumName, $description )
 		 	echo 'trying:';
 			print '/'+$FBPAGE_ID+'/p'; 
 
-		   $response = (new FacebookRequest(      $session, 'POST', '/me/albums', array(
+		   $response = (new FacebookRequest(      $session, 'POST', "/$FBPAGE_ID/albums", array(
 			'name' => $albumName,
-			'message' => $description
-			'backdated_time' => '2013-03-06T15:18:26-08:00';
+			'message' => $description,
+			'backdated_time' => '2013-03-06T15:18:26',
+			'created_time' => '2013-03-06T15:18:26',
+			'updated_time' => '2013-03-06T15:18:26'
 		//	'privacy' => $albumName,
 		      )
 		    ))->execute()->getGraphObject();
